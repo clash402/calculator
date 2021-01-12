a5b6e2a77b0ad01550fcf6fd90720673e697f792
Josh Courtney <joshrcourtney@gmail.com>
2021-01-12 15:18:29 -0600

    Refactor code. Add additional while loop.

--- main.py
+++ main.py
@@ -10,37 +10,35 @@ def divide(n1, n2): return n1 / n2
 # METHODS
 def calculate():
     ops = {"+": add, "-": subtract, "*": multiply, "/": divide}
-    n1 = float(input("What's the first number? "))
+    calculator_is_in_use = True
 
-    for op in ops:
-        print(op)
+    while calculator_is_in_use:
+        operation_is_in_progress = True
+        n1 = float(input("What's the first number? "))
+        
+        for op in ops:
+            print(op)
 
-    calculation_is_in_progress = True
+        while operation_is_in_progress:
+            op = input("Choose an operation: ")
 
-    while calculation_is_in_progress:
-        op = input("Choose an operation: ")
+            if op not in list(ops.keys()):
+                print(f"ERROR: {op} is not a valid operation")
+                continue
 
-        if op not in list(ops.keys()):
-            print(f"ERROR: {op} is not a valid operation")
-            continue
+            n2 = float(input("What's the next number? "))
+            calc = ops[op]
+            answer = calc(n1, n2)
 
-        n2 = float(input("What's the next number? "))
-        calc = ops[op]
-        answer = calc(n1, n2)
+            print(f"{n1} {op} {n2} = {answer}")
 
-        print(f"{n1} {op} {n2} = {answer}")
+            if input("Continue operating? (y/n) ").lower() == "y":
+                n1 = answer
+            else:
+                operation_is_in_progress = False
 
-        will_continue_operating = input("Continue operating? (y/n) ").lower()
-
-        if will_continue_operating == "y":
-            n1 = answer
-        else:
-            calculation_is_in_progress = False
-            will_repeat = input("Perform another calculation? (y/n) ").lower()
-
-            if will_repeat == "y":
-                # clear()
-                calculate()
+        if input("Perform another calculation? (y/n) ").lower() != "y":
+            calculator_is_in_use = False
 
 
 # MAIN
